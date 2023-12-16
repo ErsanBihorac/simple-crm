c10eb83ab83b89e17aa8731020520ffd1d8cab6c
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-12-16 13:44:48 +0100

    saves

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit, inject, } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
-import { Firestore, addDoc, collection, getFirestore, AngularFirestore } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, getFirestore, } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { initializeApp } from '@angular/fire/app';
 
@@ -14,16 +14,19 @@ import { initializeApp } from '@angular/fire/app';
 
 export class UserComponent implements OnInit {
   user = new User();
+  allUsers:any = [];
+
   app = initializeApp(environment.firebase);
   db = getFirestore(this.app);
   myCollection = collection(this.db, 'users');
+  firestore: Firestore = inject(Firestore);
 
-  constructor(public dialog: MatDialog, private firestore: AngularFirestore) {
+  constructor(public dialog: MatDialog,) {
     this.user.firstName
   }
 
   ngOnInit(): void {
-    this.firestore.collection('users')
+    collection(this.firestore, 'users')
       .valueChanges()
       .subscribe((changes: any) => {
         console.log('Received changes from DB', changes)
